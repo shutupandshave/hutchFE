94944ed35a41e311e22be8185c4e9c4c4c1c3691
James Lawrence <james@shutupandshave.com>
2020-10-08 15:22:02 +0100

    Fixed tests and added a not null test for the service

--- adminportal/src/app/app.component.spec.ts
+++ adminportal/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,16 +21,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'adminportal'`, () => {
+  it(`should have as title 'Hutch FE Test - Admin Portal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('adminportal');
+    expect(app.title).toEqual('Hutch FE Test - Admin Portal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('adminportal app is running!');
+    const title = fixture.debugElement.query(By.css('h1')).nativeElement;
+    expect(title.textContent).toContain('Hutch FE Test - Admin Portal');
   });
 });
--- adminportal/src/app/services/portal-sync.service.spec.ts
+++ adminportal/src/app/services/portal-sync.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PortalSyncService } from './portal-sync.service';
+import truthy = jasmine.truthy;
+import { PortalSync } from '../interfaces/responses/PortalSync';
+import empty = jasmine.empty;
 
 describe('PortalSyncService', () => {
   let service: PortalSyncService;
@@ -13,4 +16,11 @@ describe('PortalSyncService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('Observable should be truthy',
+    (done: DoneFn) => {
+      service.getPortalSync('appVersion', 'dataVersion', 'adminToken').subscribe(data => {
+        expect((Object.keys(data)).length).toBeGreaterThan(0)
+        done();
+      });
+    });
 });
